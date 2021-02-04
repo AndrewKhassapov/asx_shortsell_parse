9d14d1d96a174f032670bfc5c7b85400f3bddca0
Andrew Khassapov <53222142+AndrewKhassapov@users.noreply.github.com>
2021-02-04 16:17:07 +1100

    Replicated js to C# website parser
    04/02/2021
    Rebuild of the .js website parser in C#.
    The website parser will continue in development via C# or python.
    
    #**Reason**#:
    JavaScript fetch() API and AJAX cannot be used to retrieve (GET via REST) cross-origin website content.
        This results in a 'CORS error': The 'Access-Control-Allow-Origin' header has a value 'https://search.asx.com.au' that is not equal to the supplied origin. Have the server send the header with a valid value, or, if an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled.

---
+++ website_parser.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+					
+public class Program
+{
+	///<summary>URL to parse data from.</summary>
+	const string fetchURL = "https://www.asx.com.au/data/shortsell.txt";
+	
+	
+	public static void Main()
+	{
+		Console.WriteLine(ParseHTML());
+	}
+	
+	protected static string ParseHTML(){
+		string htmlBody = GetHTML().Result;
+		string[] parsedHTML = htmlBody.Split('\n');
+
+		foreach(string i in parsedHTML){
+			Console.WriteLine(i + "\n");
+		}
+		
+		return htmlBody;
+	}
+
+	protected static async Task<String> GetHTML(){
+	var client = new HttpClient();
+	// Create the HttpContent for the form to be posted.
+	var requestContent = new FormUrlEncodedContent(new [] {
+    new KeyValuePair<string, string>("text", "Text block"),
+	});
+
+	// Get the response.
+	HttpResponseMessage response = await client.PostAsync(fetchURL, requestContent);
+
+	// Get the response content.
+	HttpContent responseContent = response.Content;
+
+	// Get the stream of the content.
+	using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+		{
+    	// Write the output.
+    	return (await reader.ReadToEndAsync());
+		}
+	}
+}
+
+public class Data{
+	protected string ticker{get; private set;}
+	protected float percentShorted{get; private set;}
+	protected float volume{get; private set;}
+	protected float capital{get; private set;}
+	
+	public Data(string line){
+		//Regex regex = new Regex("([\.\,\:a-zA-Z0-9_])");
+
+	}
+}
